a1bae9fb9d2bb538c1522ca49154a2c39ec775d5
Bruno Cabral <cabral.brc@gmail.com>
2025-10-02 16:06:13 -0700

    move based on player direction

--- code/player.cpp
+++ code/player.cpp
@@ -6,37 +6,46 @@ Player::Player()
     x = Settings::GetInstance()->player_pos.x;
     y = Settings::GetInstance()->player_pos.y;
     angle = Settings::GetInstance()->player_angle;
+    player_speed = Settings::GetInstance()->player_speed;
+    player_rotation = Settings::GetInstance()->player_rotation;
 }
 
-rg::math::Vector2 Player::pos()
+rg::math::Vector2 Player::pos() const
 {
     return {x, y};
 }
 
 void Player::movement(const float dt)
 {
+    auto sin_a = sinf(angle);
+    auto cos_a = cosf(angle);
+
     if (rl::IsKeyDown(rl::KEY_W))
     {
-        y -= Settings::GetInstance()->player_speed * dt;
+        x += player_speed * dt * cos_a;
+        y += player_speed * dt * sin_a;
     }
     if (rl::IsKeyDown(rl::KEY_S))
     {
-        y += Settings::GetInstance()->player_speed * dt;
+        x -= player_speed * dt * cos_a;
+        y -= player_speed * dt * sin_a;
     }
     if (rl::IsKeyDown(rl::KEY_A))
     {
-        x -= Settings::GetInstance()->player_speed * dt;
+        x += player_speed * dt * sin_a;
+        y -= player_speed * dt * cos_a;
     }
     if (rl::IsKeyDown(rl::KEY_D))
     {
-        x += Settings::GetInstance()->player_speed * dt;
+        x -= player_speed * dt * sin_a;
+        y += player_speed * dt * cos_a;
     }
     if (rl::IsKeyDown(rl::KEY_LEFT))
     {
-        angle -= Settings::GetInstance()->player_rotation * dt;
+        angle -= player_rotation * dt;
     }
     if (rl::IsKeyDown(rl::KEY_RIGHT))
     {
-        angle += Settings::GetInstance()->player_rotation * dt;
+        angle += player_rotation * dt;
     }
 }
--- code/player.hpp
+++ code/player.hpp
@@ -9,11 +9,13 @@ public:
     Player();
     ~Player() = default;
 
-    rg::math::Vector2 pos();
+    rg::math::Vector2 pos() const;
     void movement(float dt);
 
     float x{};
     float y{};
     float angle{};
+    float player_speed{};
+    float player_rotation{};
 
 };
